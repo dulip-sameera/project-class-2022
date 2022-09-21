0fd6eaed7555a890e0de6a7a18b5f6bcc9a34ef1
Dulip Sameera <dsgitrepo101@gmail.com>
2022-09-21 13:47:28 +0530

    setup the files

---
+++ 06-19/1/fruit.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+
+if (!(isset($_SESSION['userName']) && time() - $_SESSION['lastActive'] < 10)) {
+    echo 'Time Out';
+    session_destroy();
+    include "./login.html";
+    die();
+}
+
+
+$_SESSION['lastActive'] = time();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fruits</title>
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        td,
+        th {
+            padding: 10px;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Welcome to Harvest Super</h1>
+    <h4>You are login as <?= $_SESSION['userName'] ?></h4>
+    <h4>(<a href="logout.php">logout</a>)</h4>
+
+    <h1>Fruits</h1>
+    <table border="1" style="border-collapse: collapse;">
+        <thead>
+            <tr>
+                <th>Item</th>
+                <th>Price</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Apple</td>
+                <td>220</td>
+            </tr>
+            <tr>
+                <td>Orange</td>
+                <td>420</td>
+            </tr>
+            <tr>
+                <td>Mango</td>
+                <td>320</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>

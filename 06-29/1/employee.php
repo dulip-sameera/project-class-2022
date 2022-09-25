fcc2e4f278668a4d3b0a57dd02a6453e20a609f0
Dulip Sameera <dsgitrepo101@gmail.com>
2022-09-25 11:00:10 +0530

    Created Employee Entity Class

---
+++ 06-29/1/employee.php
@@ -0,0 +1,54 @@
+<?php
+
+class Employee
+{
+
+    public $id;
+    public $name;
+    public $age;
+    public $gender;
+
+    public function __construct()
+    {
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+    }
+}

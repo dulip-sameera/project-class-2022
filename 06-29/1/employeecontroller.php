7e50ca3f0d4ae05cf5b6d3279fca6c3c1171b40d
Dulip Sameera <dsgitrepo101@gmail.com>
2022-09-26 13:00:20 +0530

    Added search methods to employeedao

--- 06-29/1/employeecontroller.php
+++ 06-29/1/employeecontroller.php
@@ -2,8 +2,32 @@
 
 require_once "employeedao.php";
 
-$employees = EmployeeDao::getAll();
+// check whether the keys have values or not 
+$hasName = !empty($_GET['name']);
+$hasGender = !empty($_GET['gender']);
 
-$json = json_encode($employees);
 
-echo $json;
+$employees = array();
+
+if ($hasName) {
+    $name = $_GET['name'];
+}
+
+if ($hasGender) {
+    $gender = $_GET['gender'];
+}
+
+// if there is no search
+if (!$hasName && !$hasGender) $employees = EmployeeDao::getAll();
+
+// if search by name
+if ($hasName && !$hasGender) $employees = EmployeeDao::getByName($name);
+
+// if search by gender
+if (!$hasName && $hasGender) $employees = EmployeeDao::getByGender($gender);
+
+// if search by name and gender
+if ($hasName && $hasGender) $employees = EmployeeDao::getByNameAndGender($name, $gender);
+
+
+echo (json_encode($employees));
